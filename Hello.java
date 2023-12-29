727d1b9d973628978b2c2fc58976b7022d67aff8
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 00:20:33 +0530

    Java basics

--- Hello.java
+++ Hello.java
@@ -1,27 +1,48 @@
-class Calculator{
+class Math {
+    public int Area(int length, int breadth) {
+        return length * breadth;
+    }
 
-    public int add(int a, int b){
-        return a+b;
+    public int Area(int side) {
+        return side * side;
     }
-    public int subtract(int a, int b){
-        return a-b;
+
+    public int Area(int length, int breadth, int height) {
+        return length * breadth * height;
     }
-    public int multiply(int a, int b){
-        return a*b;
+
+    public int AreaC(int radius) {
+        return (int) 3.14 * radius * radius;
     }
-    public int divide(int a, int b){
-        return a/b;
-    }
+
+    // These are the methods of the class Math
 }
 
-public class Hello {
+class Hello {
     public static void main(String[] arguments) {
-        System.out.println("Hello, world!");
+        // JDK -> JRE -> JVM
+        // JDK: Java Development Kit : Its a software development environment used for
+        // developing Java applications and applets.
+        // JRE: Java Runtime Environment : Its a set of software tools which are used
+        // for developing Java applications.
+        // JVM: Java Virtual Machine : Its an abstract machine. It is a specification
+        // that provides runtime environment in which java bytecode can be executed.
 
-        Calculator calc = new Calculator();
-        System.out.println("1 + 2 = " + calc.add(1, 2));
-        System.out.println("1 - 2 = " + calc.subtract(1, 2));
-        System.out.println("1 * 2 = " + calc.multiply(1, 2));
-        System.out.println("1 / 2 = " + calc.divide(1, 2));
+        // Write in one line
+        // Java is a platform independent, strongly typed, object oriented,
+        // multi-threaded, distributed and robust language.
+
+        // Function Implementation
+        // access_specifier return_type function_name (arguments) {
+        // // body
+        // }
+
+
+        Math m = new Math();
+        System.out.println(m.Area(10, 20));
+        System.out.println(m.Area(10));
+        System.out.println(m.Area(10, 20, 30));
+        System.out.println(m.AreaC(10));
+        
     }
 }
