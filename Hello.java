beac2939d1c46a4b0d017f794a563c373cca5c02
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 00:59:15 +0530

    Jagged n Multidimensional Array in Java

--- Hello.java
+++ Hello.java
@@ -1,29 +1,44 @@
 class Hello {
     public static void main(String[] arguments) {
-        int[][] a = new int[4][4];
-        // Row is compulsory, column is optional
+        int[][] a = new int[3][]; // Jagged array
+
+        a[0] = new int[3];
+        a[1] = new int[2];
+        a[2] = new int[1];
 
         for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                int random = (int) (Math.random() * 100);
-                a[i][j] = random;
-            }
-        }
-
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a[i].length; j++) {
-                System.out.print(a[i][j] + "\t");
-                // \t is tab
+            for (int j = 0; j < a[i].length ; j++) {
+                a[i][j] = i + j;
+                System.out.print(a[i][j] + " ");
             }
             System.out.println();
         }
-        System.out.println("Enhanced for loop");
-        // Enhanced for loop
-        for (int[] row : a) {
+
+        int[][] b = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}}; // Rectangular array
+
+        for (int[] row : b) {
             for (int element : row) {
-                System.out.print(element + "\t");
+                System.out.print(element + " ");
             }
             System.out.println();
         }
+
+        int[][][] c = new int[3][2][]; // 3D array
+
+        c[0][0] = new int[3];
+        c[0][1] = new int[2];
+        c[1][0] = new int[3];
+
+        for (int i = 0; i < c.length; i++) {
+            for (int j = 0; j < c[i].length ; j++) {
+                for (int k = 0; k < c[i][j].length; k++) {
+                    c[i][j][k] = i + j + k;
+                    System.out.print(c[i][j][k] + " ");
+                }
+                System.out.println();
+            }
+            System.out.println();
+        }
+
     }
 }
