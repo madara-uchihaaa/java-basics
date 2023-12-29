9ce3e766bf0ca18e22016ca505189d2e873a0652
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 00:29:36 +0530

    Stack vs Heap in JVM

--- Hello.java
+++ Hello.java
@@ -1,48 +1,16 @@
-class Math {
-    public int Area(int length, int breadth) {
-        return length * breadth;
-    }
-
-    public int Area(int side) {
-        return side * side;
-    }
-
-    public int Area(int length, int breadth, int height) {
-        return length * breadth * height;
-    }
-
-    public int AreaC(int radius) {
-        return (int) 3.14 * radius * radius;
-    }
-
-    // These are the methods of the class Math
-}
-
 class Hello {
     public static void main(String[] arguments) {
-        // JDK -> JRE -> JVM
-        // JDK: Java Development Kit : Its a software development environment used for
-        // developing Java applications and applets.
-        // JRE: Java Runtime Environment : Its a set of software tools which are used
-        // for developing Java applications.
-        // JVM: Java Virtual Machine : Its an abstract machine. It is a specification
-        // that provides runtime environment in which java bytecode can be executed.
+        // Inside JVM stack and heap used to store data
+        // Stack stores local variables and method calls
+        // Heap stores objects and arrays
 
-        // Write in one line
-        // Java is a platform independent, strongly typed, object oriented,
-        // multi-threaded, distributed and robust language.
-
-        // Function Implementation
-        // access_specifier return_type function_name (arguments) {
-        // // body
-        // }
-
-
-        Math m = new Math();
-        System.out.println(m.Area(10, 20));
-        System.out.println(m.Area(10));
-        System.out.println(m.Area(10, 20, 30));
-        System.out.println(m.AreaC(10));
-        
+        // Stack VS Heap
+        // Stack is faster than heap
+        // Stack is limited in size, heap is not
+        // Stack is thread specific, heap is not
+        // Stack is cleaned up automatically, heap is not
+        // Use case 
+        // Stack: local variables, method calls
+        // Heap: objects, arrays
     }
 }
