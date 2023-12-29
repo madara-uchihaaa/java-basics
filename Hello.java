38202a2729be90cebf82b03fa9a94ca1d2010450
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 01:13:49 +0530

    String and methods

--- Hello.java
+++ Hello.java
@@ -1,29 +1,46 @@
-class Student{
-    String name;
-    Integer age;
-
-    public Student(String name, Integer age){
-        this.name = name;
-        this.age = age;
-    }
-}
-
 class Hello {
     public static void main(String[] arguments) {
-        
-        Student student1 = new Student("John", 20);
-        Student student2 = new Student("Mary", 21);
-        Student student3 = new Student("Peter", 22);
+        // Strings in java;
 
-        // Student[] students = {student1, student2, student3};
-        Student[] students = new Student[3];
+        // Method 1
+        String greeting = "Hello 1";
+        System.out.println(greeting);
 
-        students[0] = student1;
-        students[1] = student2;
-        students[2] = student3;
+        // Method 2
+        String greeting2 = new String("Hello 2");
+        System.out.println(greeting2);
 
-        for (Student student : students) {
-            System.out.println(student.name + " is " + student.age + " years old.");
-        }
+        // Method 3
+        String greeting3 = new String();
+        System.out.println(greeting3);
+        // Result: <empty string>
+
+        // Method 4
+        String greeting4 = new String(new char[] { 'H', 'e', 'l', 'l', 'o', ' ', '4' });
+        System.out.println(greeting4);
+
+        // Method 5
+        String greeting5 = new String(new byte[] { 72, 101, 108, 108, 111, 32, 53 });
+        System.out.println(greeting5);
+
+        // Method 6
+        System.out.println(greeting + "->" + greeting2);
+        // Result: Hello 1->Hello 2
+
+        System.out.println(greeting.charAt(0));
+        // Result: H
+
+        System.out.println(greeting.length());
+        // Result: 6
+
+        System.out.println(greeting.substring(0, 3));
+        // Result: Hel
+
+        System.out.println(greeting.indexOf("l"));
+        // Result: 2
+
+        System.out.println(greeting.concat(greeting5));
+        // Result: Hello 1Hello 5
+
     }
 }
