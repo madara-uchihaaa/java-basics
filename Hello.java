9f5e2c3127569ff870312dfa69f8fd4c08d83d88
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 01:05:29 +0530

    Array of objects

--- Hello.java
+++ Hello.java
@@ -1,10 +1,29 @@
+class Student{
+    String name;
+    Integer age;
+
+    public Student(String name, Integer age){
+        this.name = name;
+        this.age = age;
+    }
+}
+
 class Hello {
     public static void main(String[] arguments) {
-        // Disadvantage of arrays
-
-        // 1: Arrays are fixed in size
-        // 2: Arrays can hold only one type of data
-        // 3: Arrays can be accessed by index only
         
+        Student student1 = new Student("John", 20);
+        Student student2 = new Student("Mary", 21);
+        Student student3 = new Student("Peter", 22);
+
+        // Student[] students = {student1, student2, student3};
+        Student[] students = new Student[3];
+
+        students[0] = student1;
+        students[1] = student2;
+        students[2] = student3;
+
+        for (Student student : students) {
+            System.out.println(student.name + " is " + student.age + " years old.");
+        }
     }
 }
