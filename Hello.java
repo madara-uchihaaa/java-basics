1819c623ecc4472fb03e8e2ca6dce84e11f809ed
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 13:36:00 +0530

    Upcasting and downcasting

--- Hello.java
+++ Hello.java
@@ -1,66 +1,22 @@
 class A {
-
-    int a;
-    int b;
-
     public void print() {
         System.out.println("A");
     }
+}
 
-    public int add(int a, int b) {
-        return a + b;
+class B extends A {
+    public void print1() {
+        System.out.println("B");
     }
-
-    // By default, it will call toString() method of object but you can override it like this
-    public String toString() {
-        return "A";
-    }
-
-    // Better to override equals() method when you override hashCode() method
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + a;
-        result = prime * result + b;
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        A other = (A) obj;
-        if (a != other.a)
-            return false;
-        if (b != other.b)
-            return false;
-        return true;
-    }
-    
 }
 
 class Hello {
     public static void main(String[] arguments) {
-        A a = new A();
-        a.a = 1;
-        a.b = 2;
+        // Upcasting and downcasting
+        A a = (A) new B(); // Upcasting
+        a.print();
 
-        A b = new A();
-        b.a = 1;
-        b.b = 2;
-
-        System.out.println(a + "->" + a.a + "->" + a.b);
-        // When you print object, it will call toString() method
-
-        Boolean c = a == b;
-        System.out.println(c);
-
-        Boolean d = a.equals(b);
-        System.out.println(d);
+        B b = (B) a; // Downcasting
+        b.print1();
     }
 }
