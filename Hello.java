6b774f22ae920747285ba381c261e7c323ec6205
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 15:45:04 +0530

    Exceptions and Errors

--- Hello.java
+++ Hello.java
@@ -1,11 +1,51 @@
 class Hello {
     public static void main(String[] arguments) {
+        // Exceptions in java;
 
-        // Types of interfaces
+        // Types of errors:
+        // 1. Syntax errors (compile time errors) - Syntax errors are detected by the compiler
+        // 2. Runtime errors - Runtime errors are detected by the JVM
+        // 3. Logical errors - Logical errors are detected by the programmer
 
-        // 1. Normal interface -> multiple abstract methods
-        // 2. Single abstract method interface (SAM) -> functional interface
-        // 3. Marker interface -> no methods
 
+        // Types of exceptions:
+        // 1. Checked exceptions
+        // 2. Unchecked exceptions
+
+
+        // Checked exceptions:
+        // 1. IOException
+        // 2. SQLException
+        // 3. ClassNotFoundException
+        // 4. InvocationTargetException
+        // 5. MalformedURLException
+        // 6. EOFException
+        // 7. FileNotFoundException
+        // 8. ParseException
+        // 9. CloneNotSupportedException
+        // 10. InterruptedException
+        // 11. NoSuchMethodException
+        // 12. NoSuchFieldException
+        // 13. AWTException
+        // 14. IllegalAccessException
+        // 15. InstantiationException
+        // 16. UnsupportedLookAndFeelException
+        // 17. UnknownHostException
+        // 18. UnknownServiceException
+        // 19. UnsupportedEncodingException
+        // 20. URISyntaxException
+        // 21. ZipException
+        // 22. DataFormatException
+        // ... and many more
+
+
+        // Unchecked exceptions:
+        // 1. ArithmeticException
+        // 2. ArrayIndexOutOfBoundsException
+        // 3. ArrayStoreException
+        // 4. ClassCastException
+        // 5. EnumConstantNotPresentException
+        // 6. IllegalArgumentException
+        // ... and many more
     }
 }
