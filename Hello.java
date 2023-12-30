8b037dd3e6a8c03242baab222c7bb4f3463e6670
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 15:17:29 +0530

    Interfaces more

--- Hello.java
+++ Hello.java
@@ -1,17 +1,48 @@
-interface Computer{
+// Interface -> Abstract Class -> Concrete Class -> Object (Instance) 
+interface Computer {
+    // By default, all methods in an interface are public and abstract
     void compute();
+
+    void config();
+
+    int age = 100; // By default, all variables in an interface are public, static, and final
 }
 
-class Laptop implements Computer{
-    public void compute(){
+interface Device{
+    void details();
+}
+
+// class -> interface -> implements
+// class -> class -> extends
+// interface -> interface -> extends
+// class -> interface -> implements -> implements
+
+class Laptop implements Computer , Device{
+    public void compute() {
         System.out.println("Laptop");
     }
+
+    public void config() {
+        System.out.println("Laptop Config");
+    }
+    public void details(){
+        System.out.println("Laptop Details");
+    }   
 }
-class Desktop implements Computer{
-    public void compute(){
+
+class Desktop implements Computer , Device{
+    public void compute() {
         System.out.println("Desktop");
     }
+
+    public void config() {
+        System.out.println("Desktop Config");
+    }
+    public void details(){
+        System.out.println("Desktop Details");
+    }
 }
+
 class Hello {
     public static void main(String[] arguments) {
         System.out.println("Hello, world!");
@@ -20,5 +51,6 @@ class Hello {
 
         Computer d = new Desktop();
         d.compute();
+
     }
 }
