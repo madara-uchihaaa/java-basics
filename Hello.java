d888da7ec30e72ad1f168ac131f2216920ab4477
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 16:47:07 +0530

    Inputs in java

--- Hello.java
+++ Hello.java
@@ -1,26 +1,46 @@
-class A {
-    public int getValue() throws ArithmeticException {
-        return 1;
-    }
-
-    public int getLength() throws NullPointerException {
-        String s = null;
-        return s.length();
-    }
-}
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
 
 class Hello {
     public static void main(String[] arguments) {
-        A a = new A();
+
+        // Method 1
+
+        System.out.println("Enter your name: ");
+        String name = System.console().readLine();
+
+        System.out.println("Hello, " + name + "!");
+
+        System.out.println("Enter your age: ");
+        int age = Integer.parseInt(System.console().readLine());
+        System.out.println("You are " + age + " years old.");
+
+        // Method 2
+
+        System.out.print("Enter your name: ");
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
         try {
-            System.out.println(a.getValue());
-            System.out.println(a.getLength());
-        } catch (ArithmeticException e) {
-            System.out.println("ArithmeticException");
-        } catch (NullPointerException e) {
-            System.out.println("NullPointerException");
+            name = br.readLine();
         } catch (Exception e) {
-            System.out.println("Exception");
+            System.out.println("Error!");
         }
+        try {
+            br.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Method 3
+
+        System.out.print("Enter your name: ");
+        Scanner sc = new Scanner(System.in);
+        Integer num = sc.nextInt();
+        System.out.println("You entered " + num);
+
+        sc.close();
     }
 }
