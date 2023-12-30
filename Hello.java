2ec0adacbb257e36609d2ad6586cdf3e7ec70b00
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 09:21:50 +0530

    Method overloading

--- Hello.java
+++ Hello.java
@@ -1,17 +1,31 @@
-
+class A{
+    public int a;
+    public void display(){
+        System.out.println("A");
+    }
+    public void show(){
+        System.out.println("A");
+    }
+}
+class B extends A{
+    public int b;
+    public void display(){
+        System.out.println("B");
+    }
+    public void show(String s){
+        System.out.println(s);
+    }
+}
 
 class Hello {
     public static void main(String[] arguments) {
-        AdvanceCalculator advanceCalculator = new AdvanceCalculator();
+        B obj = new B();
+        obj.display();
+        obj.show();
+        // Since show() is in A only and not in B, it will call the show() of A
+        // But if we pass a string in show() of B, it will call the show() of B
+        obj.show("B class");
 
-        System.out.println("Power of 2^3 is " + advanceCalculator.power(2, 3));
-        advanceCalculator.power(0, 0);
-
-        // To access Calculator class, we need to create an object of Calculator class
-        // or extend Calculator class
-
-        advanceCalculator.add(4, 0);
-
-        // In java multiple inheritance is not allowed
+        // B is a child class of A and methods of A are overridden in B
     }
 }
