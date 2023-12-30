3e41e7d2d61b3a811255256fe266ac49a19efe0b
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 13:42:22 +0530

    Wrapper class

--- Hello.java
+++ Hello.java
@@ -1,22 +1,49 @@
-class A {
-    public void print() {
-        System.out.println("A");
-    }
-}
-
-class B extends A {
-    public void print1() {
-        System.out.println("B");
-    }
-}
-
 class Hello {
     public static void main(String[] arguments) {
-        // Upcasting and downcasting
-        A a = (A) new B(); // Upcasting
-        a.print();
+        // Wrapper class for primitive type
 
-        B b = (B) a; // Downcasting
-        b.print1();
+        // Integer
+        Integer i = new Integer(10);
+
+        // Double
+        Double d = new Double(10.5);
+
+        // Character
+        Character c = new Character('a');
+
+        // Boolean
+        Boolean b = new Boolean(true);
+
+        // Byte
+        Byte by = new Byte((byte) 10);
+
+        // Short
+        Short s = new Short((short) 10);
+
+        // Long
+        Long l = new Long(10);
+
+        System.out.println(i);
+        System.out.println(d);
+        System.out.println(c);
+        System.out.println(b);
+        System.out.println(by);
+        System.out.println(s);
+        System.out.println(l);
+
+        Integer x = 10; // No need to use new keyword
+
+        int num = 7;
+        Integer y = num; // Autoboxing -> Integer y = new Integer(num);
+
+        int z = y; // auto-Unboxing -> int z = y.intValue();
+
+        // Autoboxing -> Defined as a primitive type but used as a wrapper class
+        // Auto-Unboxing -> Defined as a wrapper class but used as a primitive type
+
+        String str = "10";
+        int a = Integer.parseInt(str); // String to int
+        System.out.println(a);
+
     }
 }
