3d2fda77c001c8248ac97ef8ffcd61eab4c567da
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 19:00:02 +0530

    Var in java

--- Hello.java
+++ Hello.java
@@ -1,13 +1,26 @@
 import java.util.List;
+
 class Hello {
     public static void main(String[] arguments) {
         List<Integer> list = List.of(1, 2, 3, 4, 5);
 
         Integer sum = list.stream()
-            .filter(i -> i % 2 == 0)
-            .map(i -> i * 2)
-            .reduce(0, (a, b) -> a + b);
+                .filter(i -> i % 2 == 0)
+                .map(i -> i * 2)
+                .reduce(0, (a, b) -> a + b);
 
         System.out.println(sum);
+
+        var sum2 = list.stream()
+                .filter(i -> i % 2 == 0)
+                .map(i -> i * 2)
+                .reduce(0, (a, b) -> a + b);
+
+        System.out.println(sum2);
+
+        // var is only for LVTI (Local Variable Type Inference)
+        // var is not a keyword, it is a reserved type name
+
+        // class var {} // error: 'var' not allowed here
     }
 }
