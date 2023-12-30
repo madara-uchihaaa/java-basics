78e394a06cf25612f9b8bc66f0e1537e5aaa2bc1
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 15:42:15 +0530

    Types of interfaces

--- Hello.java
+++ Hello.java
@@ -1,61 +1,11 @@
-@FunctionalInterface
-interface HelloInterface {
-    public void sayHello();
-    // Error: Interface abstract methods cannot have body (JDK 1.8)
-}
-
-@FunctionalInterface 
-interface LaptopInterface{
-    public void sayLaptop(String Name);
-}
-
-@FunctionalInterface
-interface Command{
-    public boolean execute(String  command);
-}
-
 class Hello {
     public static void main(String[] arguments) {
 
-        // Anonymous class
-        HelloInterface hello = new HelloInterface() {
-            public void sayHello() {
-                System.out.println("Hello World! 1");
-            }
-        };
-        hello.sayHello();
+        // Types of interfaces
 
-        // Lambda expression
-        HelloInterface hello2 = () -> System.out.println("Hello World! 2");
-        hello2.sayHello();
+        // 1. Normal interface -> multiple abstract methods
+        // 2. Single abstract method interface (SAM) -> functional interface
+        // 3. Marker interface -> no methods
 
-        // Lambda expression with multiple statements
-        HelloInterface hello3 = () -> {
-            System.out.println("Hello World! 3");
-            System.out.println("Hello World! 4");
-        };
-        hello3.sayHello();
-
-
-        // Lambda expression with parameters
-        HelloInterface hello4 = () -> {
-            System.out.println("Hello World! 5");
-            System.out.println("Hello World! 6");
-        };
-        hello4.sayHello();
-
-        // Lambda expression with parameters
-        LaptopInterface laptop = (name) -> {
-            System.out.println("Hello " + name);
-        };
-        laptop.sayLaptop("Dell");
-
-        // Lambda expression with parameters
-        Command command = (cmd) -> {
-            System.out.println("Executing " + cmd);
-            return true;
-        };
-        command.execute("ls -l");
-        System.out.println(command.execute("ls -l"));
     }
 }
