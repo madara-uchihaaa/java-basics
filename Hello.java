c6f36c59e9880b5de4f2bbcb4967463797524eb6
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 17:41:02 +0530

    Threads in Java

--- Hello.java
+++ Hello.java
@@ -1,29 +1,49 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-
-class Hello {
-    public static void main(String[] arguments) {
-
-        int i = 0;
-        int j = 0;
-        BufferedReader br = null;
-        try {
-            // int res = i / j;
-
-            br = new BufferedReader(new InputStreamReader(System.in));
-
-            Integer c = Integer.parseInt(br.readLine());
-            System.out.println("You entered: " + c);
-        } catch (Exception e) {
-            System.out.println("Exception" + e.getMessage());
-        } finally {
-            System.out.println("Hello World!");
+class A extends Thread {
+    // In thread a run method is there and we have to override it.
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("A->" + i);
+            // Wait
             try {
-                br.close();
-            } catch (IOException e) {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+    }
+}
+
+class B extends Thread {
+    public void run() {
+        for (int i = 0; i < 100; i++) {
+            System.out.println("B->" + i);
+            // Wait
+            try {
+                Thread.sleep(10);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+class Hello {
+    public static void main(String[] arguments) {
+        // Threads in Java
+
+        A a = new A();
+        a.start();
+
+        System.out.println(a.getPriority() + "a.priority");
+
+        // You can also set the priority of the thread
+        // a.setPriority(10);   // 1-10 , through this you are suggesting the scheduler to give more time to this thread.
+
+        B b = new B();
+        b.start();
+        System.out.println(b.getPriority() + "b.priority");
+
+        // These things can be run in parallel.
+        // Make class a thread by extending Thread class
     }
 }
