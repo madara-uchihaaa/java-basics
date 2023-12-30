754ea2c6ca5eaea46e06aa229998c91332c2f7b0
Rishabh Verma <rishabh.v@clappia.com>
2023-12-30 15:20:53 +0530

    Enums in Java

--- Hello.java
+++ Hello.java
@@ -1,56 +1,25 @@
-// Interface -> Abstract Class -> Concrete Class -> Object (Instance) 
-interface Computer {
-    // By default, all methods in an interface are public and abstract
-    void compute();
-
-    void config();
-
-    int age = 100; // By default, all variables in an interface are public, static, and final
+enum Color {
+    RED, GREEN, BLUE;
 }
-
-interface Device{
-    void details();
-}
-
-// class -> interface -> implements
-// class -> class -> extends
-// interface -> interface -> extends
-// class -> interface -> implements -> implements
-
-class Laptop implements Computer , Device{
-    public void compute() {
-        System.out.println("Laptop");
-    }
-
-    public void config() {
-        System.out.println("Laptop Config");
-    }
-    public void details(){
-        System.out.println("Laptop Details");
-    }   
-}
-
-class Desktop implements Computer , Device{
-    public void compute() {
-        System.out.println("Desktop");
-    }
-
-    public void config() {
-        System.out.println("Desktop Config");
-    }
-    public void details(){
-        System.out.println("Desktop Details");
-    }
-}
-
 class Hello {
     public static void main(String[] arguments) {
-        System.out.println("Hello, world!");
-        Computer c = new Laptop();
-        c.compute();
+        System.out.println("Hello, World!");
+        System.out.println(Color.RED);
+        System.out.println(Color.GREEN);
+        System.out.println(Color.BLUE);
 
-        Computer d = new Desktop();
-        d.compute();
+        Color c = Color.RED;
+        Color res = switch (c) {
+            case RED -> Color.GREEN;
+            case GREEN -> Color.BLUE;
+            case BLUE -> Color.RED;
+        };
+        System.out.println(res);
+        System.out.println(Color.RED.ordinal());
 
-    }
+        Color[] colors = Color.values();
+        for (Color color : colors) {
+            System.out.println(color);
+        }
+    }   
 }
